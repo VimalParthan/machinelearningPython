78b7761a2dadd6aef5e013319617f2305140b515
Vimal Parthan <vimalparthan@gmail.com>
2017-07-21 10:23:16 +0530

    python template edited

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -1,23 +1,27 @@
-# Data Preprocessing Template
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 21 08:13:25 2017
 
+@author: Vimal
+"""
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+#Importing the dataset
 dataset = pd.read_csv('Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+X = dataset.iloc[:,:-1].values
+Y = dataset.iloc[:,3].values
+
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, Y_train , Y_test = train_test_split(X,Y,test_size = 0.2, random_state = 0) 
 
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+""""from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+X_test = sc_X.transform(X_test)"""
